7c71a78c4781468f2495b6a11833710c0e249d13
slaveworx <tgalvao@sapo.pt>
2023-03-23 21:47:55 +0000

    - removed some garbage

--- config/custom-post-types.php
+++ config/custom-post-types.php
@@ -1,4 +1,5 @@
 <?php
+namespace TG;
 //****************************************
                                         
 // 🆃🅶                                     
@@ -8,8 +9,9 @@
                                         
 //****************************************
 
+//Register your Custom Post types here...
 
-TG\TG::create_cpt(
+TG::create_cpt(
     "portfolio",
     array(
         'menu_position'     => 0,
@@ -17,25 +19,4 @@ TG\TG::create_cpt(
         'supports'          => array('title', 'page-attributes', 'revisions')
     ),
     array('menu_name' => _x('Portfolio', THEME_TEXT_DOMAIN))
-);
-
-TG\TG::create_cpt(
-    "technology",
-    array(
-        'menu_position'     => 0,
-        'menu_icon'         => 'dashicons-media-code',
-        'supports'          => array('title', 'page-attributes', 'revisions')
-    ),
-    array('menu_name' => _x('Technologies', THEME_TEXT_DOMAIN))
-);
-
-TG\TG::create_cpt(
-    "course",
-    array(
-        'menu_position'     => 0,
-        'menu_icon'         => 'dashicons-welcome-learn-more',
-        'supports'          => array('title', 'page-attributes'),
-        'has_archive'       => false
-    ),
-    array('menu_name' => _x('Courses', THEME_TEXT_DOMAIN))
 );

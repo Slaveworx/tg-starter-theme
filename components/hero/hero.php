91057105cc535d6b7089a58fa03586ee0a5b556b
Tiago M Galvão <tgalvao@sapo.pt>
2023-10-06 15:53:50 +0100

    Implements Task 2 from issue #16

--- components/hero/hero.php
+++ components/hero/hero.php
@@ -16,14 +16,17 @@
 //                PROPS                  *
 //****************************************
 // Define your component props here
-// ### Example: $message = $message ?? "No message was passed to the component";
+$hero_title = $hero_title ?? "TG Starter Theme";
+$hero_description = $hero_description ?? "No message was passed to the component";
+$btn_link = $btn_link ?? "#";
+$btn_text = $btn_text ?? "Get Started";
 ?>
 
 <section class="hero" data-row-index="<?php echo $row_index; ?>">
     <div class="hero__overlay"></div>
     <div class="container-small">
-        <h1 class="hero__title"><?php echo $title; ?></h1>
-        <p class="hero__description"><?php echo $description; ?></p>
+        <h1 class="hero__title"><?php echo $hero_title; ?></h1>
+        <p class="hero__description"><?php echo $hero_escription; ?></p>
         <a href="<?php echo $btn_link; ?>" class="hero__cta btn__primary"><?php echo $btn_text; ?></a>
     </div>
 </section>
